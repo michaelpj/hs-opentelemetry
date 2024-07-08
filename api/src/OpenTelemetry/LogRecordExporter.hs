ab8e08a58c4a3c3ecb0b44e4ddb5914c4a43712e
evanlauer1 <evanlauer@mercury.com>
2024-07-08 13:00:01 -0700

    Added LogRecordExporters and updated documentation.

---
+++ api/src/OpenTelemetry/LogRecordExporter.hs
@@ -0,0 +1,8 @@
+module OpenTelemetry.LogRecordExporter (
+  LogRecordExporter (..),
+  ShutdownResult (..),
+) where
+
+import OpenTelemetry.Internal.Logs.Types (LogRecordExporter (..))
+import OpenTelemetry.LogRecordProcessor (ShutdownResult (..))
+
